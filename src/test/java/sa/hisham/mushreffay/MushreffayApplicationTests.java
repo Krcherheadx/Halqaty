a60e90ac977f52c548f29114b608e014fb7ed19f
Hisham Asluhaibani <H.AlSuhaibani@Futurework.com.sa>
2025-07-22 10:45:23 +0300

    setting-up the skeleton app

---
+++ src/test/java/sa/hisham/mushreffay/MushreffayApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.hisham.mushreffay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MushreffayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
