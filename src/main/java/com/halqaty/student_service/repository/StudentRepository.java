5257168b3e699be2a3324acc6db61dc29b760057
Hisham Asluhaibani <H.AlSuhaibani@Futurework.com.sa>
2025-09-24 17:27:08 +0300

    moving to student-service

--- src/main/java/com/halqaty/student_service/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.halqaty.student_service.repository;
-
-import com.halqaty.student_service.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, String> {
-
-}
