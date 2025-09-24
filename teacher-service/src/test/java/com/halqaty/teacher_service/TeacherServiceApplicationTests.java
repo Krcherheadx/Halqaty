3809bd29a12bba50510b71a587dfd38ce04f73d1
Hisham Asluhaibani <H.AlSuhaibani@Futurework.com.sa>
2025-09-24 17:39:59 +0300

    Set-up teacher service

---
+++ teacher-service/src/test/java/com/halqaty/teacher_service/TeacherServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.halqaty.teacher_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class TeacherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
