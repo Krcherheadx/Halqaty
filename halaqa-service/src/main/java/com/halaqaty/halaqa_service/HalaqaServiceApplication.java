6c42c833f85f0c46a926c1b8fe74def054a0d8f7
Hisham Asluhaibani <H.AlSuhaibani@Futurework.com.sa>
2025-09-25 13:39:27 +0300

    Setting-up Halaqa-service

---
+++ halaqa-service/src/main/java/com/halaqaty/halaqa_service/HalaqaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.halaqaty.halaqa_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalaqaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalaqaServiceApplication.class, args);
+	}
+
+}
